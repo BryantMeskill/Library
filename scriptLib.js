997a73e945cd459b98f52fc1bb18833a16d9da78
Bryant Meskill <bryant.meskill@gmail.com>
2022-01-16 12:31:01 -0600

    Initial commit.

---
+++ scriptLib.js
@@ -0,0 +1,12 @@
+let theLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBook() {}
+
+function displayBooks() {}
