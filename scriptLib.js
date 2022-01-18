da0171ee54fb332605763672be7c241763162568
Bryant Meskill <bryant.meskill@gmail.com>
2022-01-18 08:07:56 -0600

    Main functions in place.

--- scriptLib.js
+++ scriptLib.js
@@ -1,5 +1,11 @@
-const addBookBtn = document.querySelector(".open-prompt");
-let theLibrary = [];
+const openPrompt = document.querySelector(".open-prompt");
+const userForm = document.querySelector(".user-form");
+const addBook = document.querySelector(".add-book");
+const titleData = document.querySelector(".book-title");
+const authorData = document.querySelector(".author");
+const pageData = document.querySelector(".pages");
+const readData = document.querySelector(".read");
+const theLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -8,9 +14,53 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-addBookBtn.addEventListener("click", () => {
-  const userForm = document.querySelector(".user-form");
+openPrompt.addEventListener("click", () => {
+  userForm.classList.remove("hidden");
+  openPrompt.classList.add("hidden");
 });
 
-function addLibraryBook() {}
-function displayBooks() {}
+addBook.addEventListener("click", () => {
+  userForm.classList.add("hidden");
+  openPrompt.classList.remove("hidden");
+  let newBook = new Book(
+    titleData.value,
+    authorData.value,
+    pageData.value,
+    readData.checked
+  );
+  addLibraryBook(newBook);
+  displayBooks(newBook);
+  titleData.value = "";
+  authorData.value = "";
+  pageData.value = "";
+  readData.checked = false;
+});
+
+function addLibraryBook(book) {
+  theLibrary.push(book);
+}
+
+function displayBooks(Book) {
+  const userBook = document.querySelector(".user-book");
+  userBook.innerHTML = "";
+  for (let i = 0; i < theLibrary.length; i++) {
+    const titleDisplay = document.createElement("div");
+    const authorDisplay = document.createElement("div");
+    const pageDisplay = document.createElement("div");
+    const readDisplay = document.createElement("div");
+    const bookDisplay = document.createElement("div");
+    bookDisplay.classList.add("book-display");
+    titleDisplay.textContent = theLibrary[i].title;
+    authorDisplay.textContent = theLibrary[i].author;
+    pageDisplay.textContent = `${theLibrary[i].pages} pages.`;
+    readDisplay.textContent = theLibrary[i].read;
+    userBook.appendChild(bookDisplay);
+    bookDisplay.appendChild(titleDisplay);
+    bookDisplay.appendChild(authorDisplay);
+    bookDisplay.appendChild(pageDisplay);
+    bookDisplay.appendChild(readDisplay);
+  }
+}
+
+//add event listener to div for checkbox
+//book removal
