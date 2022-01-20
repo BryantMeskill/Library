f129221a34f01df1aeb70782be41233cb9c55b91
Bryant Meskill <bryant.meskill@gmail.com>
2022-01-19 19:09:37 -0600

    Book removal added. Array cleanup implemented.

--- scriptLib.js
+++ scriptLib.js
@@ -22,7 +22,7 @@ openPrompt.addEventListener("click", () => {
 addBook.addEventListener("click", () => {
   userForm.classList.add("hidden");
   openPrompt.classList.remove("hidden");
-  let newBook = new Book(
+  const newBook = new Book(
     titleData.value,
     authorData.value,
     pageData.value,
@@ -40,27 +40,50 @@ function addLibraryBook(book) {
   theLibrary.push(book);
 }
 
-function displayBooks(Book) {
+function displayBooks() {
   const userBook = document.querySelector(".user-book");
   userBook.innerHTML = "";
   for (let i = 0; i < theLibrary.length; i++) {
     const titleDisplay = document.createElement("div");
     const authorDisplay = document.createElement("div");
     const pageDisplay = document.createElement("div");
-    const readDisplay = document.createElement("div");
     const bookDisplay = document.createElement("div");
+    const removeBtn = document.createElement("button");
+    const checkboxLabel = document.createElement("label");
+    const readCheckbox = document.createElement("input");
+
+    checkboxLabel.classList.add("checkbox-label");
+    checkboxLabel.appendChild(document.createTextNode("Book Read: "));
+    readCheckbox.setAttribute("type", "checkbox");
+    readCheckbox.classList.add("check");
+    removeBtn.classList.add("remove-button");
     bookDisplay.classList.add("book-display");
-    titleDisplay.textContent = theLibrary[i].title;
-    authorDisplay.textContent = theLibrary[i].author;
-    pageDisplay.textContent = `${theLibrary[i].pages} pages.`;
-    readDisplay.textContent = theLibrary[i].read;
+
+    titleDisplay.textContent = `Title: "${theLibrary[i].title}"`;
+    authorDisplay.textContent = `Author: ${theLibrary[i].author}`;
+    pageDisplay.textContent = `Length: ${theLibrary[i].pages} pages.`;
+    removeBtn.innerText = "Remove Book";
+
+    if (theLibrary[i].read === true) {
+      readCheckbox.setAttribute("checked", true);
+    }
+
     userBook.appendChild(bookDisplay);
     bookDisplay.appendChild(titleDisplay);
     bookDisplay.appendChild(authorDisplay);
     bookDisplay.appendChild(pageDisplay);
-    bookDisplay.appendChild(readDisplay);
+    bookDisplay.appendChild(checkboxLabel);
+    bookDisplay.appendChild(readCheckbox);
+    bookDisplay.appendChild(removeBtn);
+
+    readCheckbox.addEventListener("change", (e) => {
+      theLibrary[i].read = e.target.checked;
+      console.log("hehe");
+    });
+
+    removeBtn.addEventListener("click", () => {
+      userBook.removeChild(bookDisplay);
+      theLibrary.splice(i, 1);
+    });
   }
 }
-
-//add event listener to div for checkbox
-//book removal
